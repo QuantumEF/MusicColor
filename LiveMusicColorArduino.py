0665ac375fbced717001de51b45c5451ac4decdc
git <git@192.168.5.88>
2019-12-03 20:55:40 -0600

    Updated the Arduino Examples to use the Library

--- LiveMusicColorArduino.py
+++ LiveMusicColorArduino.py
@@ -5,32 +5,12 @@ from colorsys import hsv_to_rgb
 import serial
 import array
 import struct
+import MusicColorLib as mcl
 
-def audio_to_color( audio_chunk_size , frequencies):
-	c = fft(audio_chunk_size)
-	d = int(len(c)/2)  # you only need half of the fft list (real signal symmetry)
-	proc = abs(c[:(d-1)])
-	max_ind = np.argmax(proc)
-	#max_val = np.max(fft_plot)
-	if frequencies[max_ind] != 0:
-		#pitch is half-steps above C0
-		pitch = 12 * np.log2( frequencies[max_ind] / 16.35)
-	else: 
-		#log of zero is imaginary, and sometimes that is a value that is returned so /shrug
-		pitch = 1.0
-	#map 0-12 0-360deg (0-1 for hsv fucntion input)
-	h = pitch % 12 #only look at octaves
-	color = hsv_to_rgb(h/12, 1, 1)
-	return color
+fs = 41000
+chunk_size = int(41000/7)
 
-fs = 44100
-division = 7
-chunk_size = int(44100/division)
-
-#k = np.arange(chunk_size)
-#T = chunk_size/fs  # where fs is the sampling frequency
-#frqLabel = k/T
-frqLabel = division*np.arange(chunk_size) #magic I am not fully able to explain yet
+color_stream = mcl.LiveColor()
 
 #This opens an audio stream from the mic
 p = pyaudio.PyAudio()
@@ -42,7 +22,7 @@ ser = serial.Serial('COM12',115200)
 while True:
 
 	b = np.frombuffer(stream.read(chunk_size),dtype=np.int16)
-	color = audio_to_color(b,frqLabel)
+	color = color_stream.color_buffer(b)
 
 	color_buf = np.array( np.multiply(255,color) ).astype(int)
 	write_bytes = struct.pack('<' + 'B' * len(color_buf),  *color_buf) 
--- MusicColorArduino.py
+++ MusicColorArduino.py
@@ -5,41 +5,9 @@ from colorsys import hsv_to_rgb
 import pygame
 import serial
 import struct
+import MusicColorLib as mcl
 
-def audio_to_color( audio_chunk , frequencies):
-	c = fft(audio_chunk)
-	d = int(len(c)/2)  # you only need half of the fft list (real signal symmetry)
-	proc = abs(c[:(d-1)])
-	max_ind = np.argmax(proc)
-	#max_val = np.max(fft_plot)
-	if frequencies[max_ind] != 0:
-		#pitch is half-steps above C0
-		pitch = 12 * np.log2( frequencies[max_ind] / 16.35)
-	else: 
-		#log of zero is imaginary, and sometimes that is a value that is returned so /shrug
-		pitch = 1.0
-	#map 0-12 0-360deg (0-1 for hsv fucntion input)
-	h = pitch % 12 #only look at octaves
-	color = hsv_to_rgb(h/12, 1, 1)
-	return color
-
-filename = "TestFiles/ColorTest5.wav"
-
-fs, data = wavfile.read(filename) # load the data
-division = 14 #division of audio
-chunk_size = int(fs/division) #the subdivisions to take the fft of 
-b = data.T[0] # this is a two channel soundtrack, I get the first track
-
-#k = np.arange(chunk_size)
-#T = chunk_size/fs  # where fs is the sampling frequency same as 1/division
-#frqLabel = k/T
-frqLabel = division*np.arange(chunk_size) #magic I am not fully able to explain yet
-
-color_array = []
-#This is to pre-process the wav file
-for chunk in range(0,len(b)-chunk_size,chunk_size):
-	color = audio_to_color( b[chunk:chunk+chunk_size] ,frqLabel)
-	color_array.append(np.multiply(255,color))
+wavcolor = mcl.wavcolor("TestFiles/ColorTest5.wav")
 
 #opens serial port to arduino. Change COM12 for whatever you need
 ser = serial.Serial('COM12',115200)
@@ -50,7 +18,7 @@ pygame.mixer.music.load(filename)
 pygame.mixer.music.play(-1)
 
 #color display
-for color in color_array:
+for color in wavcolor.colors:
 	color_buf = np.array( color ).astype(int)
 	#forces the values to be bytes so it can be sent over serial
 	write_bytes = struct.pack('<' + 'B' * len(color_buf),  *color_buf) 
